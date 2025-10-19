16870318d13366436dfcd2c0d6ab2a3e884e5342
mayconmatuoka1-lang <maycon.matuoka1@gmail.com>
2025-10-19 05:01:48 +0000

    resolucao mod1 ex1

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,21 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
+nome = 'A'
+
+objeto = "B"
+
+exemplo = '''
+quando a string normalmente
+tem mais de uma linha
+'''
 
 # Declare variáveis de cada tipo numérico
+int = 1
 
+float = 1,1
 
 # Declare uma variável do tipo booleano
+verde = True
+
+vermelho = False
